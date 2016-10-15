2c5a5af329d802882fe954264fb6a3d54294c40c
Rob Spectre <rob@brooklynhacker.com>
2016-10-15 14:45:09 -0400

    Moved local settings import to end of file to override defaults.wq

--- humantrafficking_tips/humantrafficking_tips/settings.py
+++ humantrafficking_tips/humantrafficking_tips/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-from .local import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +131,5 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+from .local import *
